913689769e201952a27c73aefea88476316e1b16
Thomas Ohms <thomas@ohms.pro>
2020-05-06 15:40:32 +0200

    feat(netstd-extensions): add DateTime and Image extensions
    Add DateTime extensions IsYesterdayOrOlderThan() and ToInternationalDateString().
    Last will output [yyyy-mm-dd].
    
    Add Image extension ResizeKeepAspect().

---
+++ NetStandardLibrary/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tom4u.Toolkit.NetStandardLibrary.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsYesterdayOrOlderThan(this DateTime date, DateTime comparingDate)
+        {
+            if (date.Year < comparingDate.Year)
+                return true;
+
+            if (date.Month < comparingDate.Month)
+                return true;
+
+            return date.Day < comparingDate.Day;
+        }
+
+        public static string ToInternationalDateString(this DateTime date)
+        {
+            return $"{date.Year}-{date.Month}-{date.Day}";
+        }
+    }
+}
---
+++ NetStandardLibrary/Extensions/ImageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Tom4u.Toolkit.NetStandardLibrary.Extensions
+{
+    public static class ImageExtensions
+    {
+        internal static Image ResizeKeepAspect(this Image image, int maxWidth, int maxHeight, bool enlarge = false)
+        {
+            maxWidth = enlarge ? maxWidth : Math.Min(maxWidth, image.Width);
+            maxHeight = enlarge ? maxHeight : Math.Min(maxHeight, image.Height);
+
+            var rnd = Math.Min(maxWidth / (decimal)image.Width, maxHeight / (decimal)image.Height);
+            var newSize = new Size((int)Math.Round(image.Width * rnd), (int)Math.Round(image.Height * rnd));
+            var newImage = (Image) new Bitmap(image, newSize);
+
+            return newImage;
+        }
+
+        internal static Image ResizeKeepAspect(this Image image, int maxSize, bool enlarge = false)
+        {
+            return ResizeKeepAspect(image, maxSize, maxSize, enlarge);
+        }
+    }
+}
